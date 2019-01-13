5ed7efbbd025fa63f2f71674b259b2274c12f06f
msuabladerr <46645739+msuabladerr@users.noreply.github.com>
2019-01-13 12:04:38 +0100

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: node topbot.js
---
+++ topbot.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(`on 1 servers | *help | By Itachayyy#2858 .`,'https://www.twitch.tv/v5bz');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
+});
+client.on('message', message => {
+     if (message.content === ".servers") {
+     let embed = new Discord.RichEmbed()
+  .setColor("#0000FF")
+  .addField("**Server: **" , client.guilds.size)
+  message.channel.sendEmbed(embed);
+    }
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+  
+  
+  
+
+});
+ client.login("NTMzOTYyODQ4MTcxMTk2NDE2.DxyrOA.ta5bYq8d1x40a0QmwPPs5om60M4");
